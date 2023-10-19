ad59d6b1e49771805379884f1dea20476fbc08f3
noahkise <noah@kisefamily.com>
2023-10-19 14:04:07 -0700

    making progress
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- character-builder/js/scripts.js
+++ character-builder/js/scripts.js
@@ -5,29 +5,32 @@
 //     event.preventDefault();
 //   });
 // });
+window.onload = function() {
 
-window.addEventListener("")
+  let form = document.querySelector("form#abilities");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    let characterType = document.querySelector("select#characterType").value;
+    let characterAbility = document.querySelector("input#abilities").value;
 
-let characterName = getElementById("characterName");
-let characterType = document.querySelector("input#characterType").value;
-let characterAbility = document.querySelector("input#abilities").value;
-
-if (characterType === "wizard" && characterAbility === "superStrength") {
-  strongWizard.removeAttribute("class");
-} else if (characterType === "wizard" && characterAbility === "mindReading") {
-  mindReadingWizard.removeAttribute("class");
-} else if (characterType === "wizard" && characterAbility === "flight") {
-  flightWizard.removeAttribute("class");
-} else if (characterType === "dwarf" && characterAbility === "superStrength") {
-  strongDwarf.removeAttribute("class");
-} else if (characterType === "dwarf" && characterAbility === "mindReading") {
-  mindReadingDwarf.removeAttribute("class");
-} else if (characterType === "dwarf" && characterAbility === "flight") {
-  flightdwarf.removeAttribute("class");
-} else if (characterType === "mermaid" && characterAbility === "superStrength") {
-  strongMermaid.removeAttribute("class");
-} else if (characterType === "mermaid" && characterAbility === "mindReading") {
-  mindReadingMermaid.removeAttribute("class");
-} else if (characterType === "mermaid" && characterAbility === "flight") {
-  flightMermaid.removeAttribute("class");
-}
+    if (characterType === "wizard" && characterAbility === "superStrength") {
+      strongWizard.removeAttribute("class");
+    } else if (characterType === "wizard" && characterAbility === "mindReading") {
+      mindReadingWizard.removeAttribute("class");
+    } else if (characterType === "wizard" && characterAbility === "flight") {
+      flightWizard.removeAttribute("class");
+    } else if (characterType === "dwarf" && characterAbility === "superStrength") {
+      strongDwarf.removeAttribute("class");
+    } else if (characterType === "dwarf" && characterAbility === "mindReading") {
+      mindReadingDwarf.removeAttribute("class");
+    } else if (characterType === "dwarf" && characterAbility === "flight") {
+      flightdwarf.removeAttribute("class");
+    } else if (characterType === "mermaid" && characterAbility === "superStrength") {
+      strongMermaid.removeAttribute("class");
+    } else if (characterType === "mermaid" && characterAbility === "mindReading") {
+      mindReadingMermaid.removeAttribute("class");
+    } else if (characterType === "mermaid" && characterAbility === "flight") {
+      flightMermaid.removeAttribute("class");
+    }
+  });
+};
