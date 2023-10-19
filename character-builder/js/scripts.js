1b75f460941aaa8001842215278f14621f60d996
noahkise <noah@kisefamily.com>
2023-10-19 13:30:51 -0700

    add if else
    Co-authored-by: Ravin Fisher <ravesterrr@gmail.com>

--- character-builder/js/scripts.js
+++ character-builder/js/scripts.js
@@ -6,3 +6,28 @@
 //   });
 // });
 
+window.addEventListener("")
+
+let characterName = getElementById("characterName");
+let characterType = document.querySelector("input#characterType").value;
+let characterAbility = document.querySelector("input#abilities").value;
+
+if (characterType === "wizard" && characterAbility === "superStrength") {
+  strongWizard.removeAttribute("class");
+} else if (characterType === "wizard" && characterAbility === "mindReading") {
+  mindReadingWizard.removeAttribute("class");
+} else if (characterType === "wizard" && characterAbility === "flight") {
+  flightWizard.removeAttribute("class");
+} else if (characterType === "dwarf" && characterAbility === "superStrength") {
+  strongDwarf.removeAttribute("class");
+} else if (characterType === "dwarf" && characterAbility === "mindReading") {
+  mindReadingDwarf.removeAttribute("class");
+} else if (characterType === "dwarf" && characterAbility === "flight") {
+  flightdwarf.removeAttribute("class");
+} else if (characterType === "mermaid" && characterAbility === "superStrength") {
+  strongMermaid.removeAttribute("class");
+} else if (characterType === "mermaid" && characterAbility === "mindReading") {
+  mindReadingMermaid.removeAttribute("class");
+} else if (characterType === "mermaid" && characterAbility === "flight") {
+  flightMermaid.removeAttribute("class");
+}
